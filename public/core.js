9360b9ee9b6a9e641239928ee63602883a37cfcb
thedigitalgarage <johnmccawley@outlook.com>
2017-02-14 17:26:58 -0700

    add tutorial files

--- public/core.js
+++ public/core.js
@@ -1,8 +1,42 @@
 // public/core.js
-var app = angular.module('helloWorld', []);
+var helloTaskList = angular.module('helloTaskList', []);
 
-app.controller('MainCtrl', [
-'$scope',
-function($scope){
-  $scope.test = 'Hello World! Your first MEAN Stack App on Digital Garage';
-}]);
+function MainCtrl($scope, $http) {
+    $scope.formData = {};
+
+    // when landing on the page, get all tasks and show them
+    $http.get('/api/tasks')
+        .success(function(data) {
+            $scope.tasks = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // when submitting the add form, send the text to the node API
+    $scope.createTask = function() {
+        $http.post('/api/tasks', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.tasks = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a task after checking it
+    $scope.deleteTask = function(id) {
+        $http.delete('/api/tasks/' + id)
+            .success(function(data) {
+                $scope.tasks = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+}
