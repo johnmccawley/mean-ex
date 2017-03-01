626f5a679729f4aa5b81c2f3ed65fda8444ec447
John McCawley <john.mccawley@thedigitalgarage.io>
2017-03-01 11:28:18 -0700

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -63,7 +63,7 @@ module.exports = function(app) {
 
     // application -------------------------------------------------------------
     app.get('*', function(req, res) {
-        res.sendFile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+        res.sendFile('./public/index.html', { root: __dirname }); // load the single view file (angular will handle the page changes on the front-end)
     });
 
 };
