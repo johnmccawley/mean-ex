e2694ec665301f18efdc54407ecba42fdfbf47b7
John McCawley <john.mccawley@thedigitalgarage.io>
2017-02-28 18:26:54 -0700

    Update http for Angular 1.6

--- public/app.js
+++ public/app.js
@@ -5,50 +5,46 @@ var helloTaskList = angular.module('helloTaskList', [])
     $scope.formData = {};
 
     // when landing on the page, get all tasks and show them
-    $http({
-      method: 'GET',
-      url: '/api/tasks'
-   }).then(function (data){
-        $scope.tasks = data;
-        console.log(data);
-   },function (data){
-        console.log('Error: ' + data);
-   });
+    $http.get('/api/tasks')
+      .then(
+          function(data){
+            // success callback
+            $scope.tasks = data;
+            console.log(data);
+          }, 
+          function(data){
+            // failure call back
+            console.log('Error: ' + data);
+          }
+       );
     
-    
-    
-//    $http.get('/api/tasks')
-//        .success(function(data) {
-//            $scope.tasks = data;
-//            console.log(data);
-//        })
-//        .error(function(data) {
-//            console.log('Error: ' + data);
-//        });
-
-    // when submitting the add form, send the text to the node API
+    // when submitting the add form, send the text to the node API 
     $scope.createTask = function() {
         $http.post('/api/tasks', $scope.formData)
-            .success(function(data) {
-                $scope.formData = {}; // clear the form so our user is ready to enter another
-                $scope.tasks = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
+            .then(
+                function(data) {
+                  $scope.formData = {}; // clear the form so our user is ready to enter another
+                  $scope.tasks = data;
+                  console.log(data);
+                },
+                function(data) {
+                  console.log('Error: ' + data);
+                }
+            );
     };
 
     // delete a task after checking it
     $scope.deleteTask = function(id) {
-        $http.delete('/api/tasks/' + id)
-            .success(function(data) {
-                $scope.tasks = data;
+      $http.delete('/api/tasks/' + id)
+         .then(
+             function(data){
+               // success callback
+               $scope.tasks = data;
                 console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
+             }, 
+             function(response){
+               // failure call back
+               console.log('Error: ' + data);
+             }
+         );
     };
-
-});
